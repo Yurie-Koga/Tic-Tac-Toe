986194e6e6d6086803115eb50dbdb88d87b0962f
Yurie-Koga <54040614+Yurie-Koga@users.noreply.github.com>
2019-11-08 00:09:29 -0800

    Add files

---
+++ build/precache-manifest.7a4a938e6475868334e7f656bbe7fa01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa23bc76b52e07a3c47501b1c4b94671",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0f5ff50055b3f2343684",
+    "url": "./static/css/main.a9827d45.chunk.css"
+  },
+  {
+    "revision": "7144a41f7e6c783a7e97",
+    "url": "./static/js/2.83a1039e.chunk.js"
+  },
+  {
+    "revision": "0f5ff50055b3f2343684",
+    "url": "./static/js/main.9cf070f6.chunk.js"
+  },
+  {
+    "revision": "887be812d90317c1619a",
+    "url": "./static/js/runtime-main.11da929d.js"
+  }
+]);
